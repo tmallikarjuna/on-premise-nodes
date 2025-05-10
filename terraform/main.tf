b17ac05c8b30f92634f218e288c5737a5d75df46
Tejesh Mallikarjuna <tej.sahara@gmail.com>
2025-05-10 22:53:22 +0000

    Execute preloaded scripts

--- terraform/main.tf
+++ terraform/main.tf
@@ -36,6 +36,37 @@ resource "google_compute_instance" "bastion_host" {
   }
 
   tags = ["bastion-host"]
+
+  metadata_startup_script = <<-EOT
+    #!/bin/bash
+    apt-get update
+    apt-get install -y isc-dhcp-server squid
+
+    # Configure DHCP server
+    cat <<EOF > /etc/dhcp/dhcpd.conf
+    default-lease-time 600;
+    max-lease-time 7200;
+
+    subnet 192.168.100.0 netmask 255.255.255.0 {
+      range 192.168.100.100 192.168.100.200;
+      option routers 192.168.100.1;
+      option domain-name-servers 8.8.8.8, 8.8.4.4;
+      option domain-name "docker.local";
+    }
+    EOF
+
+    systemctl restart isc-dhcp-server
+
+    # Configure Squid proxy
+    cat <<EOF > /etc/squid/squid.conf
+    http_port 3128
+    acl localnet src 192.168.100.0/24
+    http_access allow localnet
+    http_access deny all
+    EOF
+
+    systemctl restart squid
+  EOT
 }
 
 resource "google_compute_instance" "private_vm" {
@@ -54,6 +85,20 @@ resource "google_compute_instance" "private_vm" {
     network    = google_compute_network.private_network.id
     subnetwork = google_compute_subnetwork.private_subnetwork.id
   }
+
+  metadata_startup_script = <<-EOT
+    #!/bin/bash
+    apt-get update
+    apt-get install -y dhclient
+
+    # Configure DHCP client
+    dhclient eth0
+
+    # Configure Squid proxy
+    echo "export http_proxy=http://192.168.100.1:3128" >> /etc/environment
+    echo "export https_proxy=http://192.168.100.1:3128" >> /etc/environment
+    source /etc/environment
+  EOT
 }
 
 resource "google_compute_firewall" "allow_ssh" {
